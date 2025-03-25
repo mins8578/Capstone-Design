cb53fdf8b6c5c399dd3632965bda19ce3d0c1ea7
seunghyuk <good011218@naver.com>
2025-03-25 17:58:41 +0900

    메인 홈페이지 수정

--- src/components/mainpage/noticebox.js
+++ src/components/mainpage/noticebox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './noticebox.css';
+import { FaArrowCircleRight } from 'react-icons/fa';
 
 function NoticeBox({ title }) {
   return (
     <div className="notice-box">
       <div className="content">{title}</div>
       <div className="bottom">
-        <div className="arrow">➡️</div>
+        <div className="arrow"><FaArrowCircleRight size={30} color="white" /></div>
       </div>
     </div>
   );
