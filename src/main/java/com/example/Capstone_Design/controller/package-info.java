0ee67ab7cbdcce7317da8320101cc8d09892eea3
김민석 <124144536+mins8578@users.noreply.github.com>
2025-05-21 20:59:53 +0900

    Revert "좋아요 로직 추가"

---
+++ src/main/java/com/example/Capstone_Design/controller/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 📌 클라이언트 요청을 처리하는 컨트롤러 클래스들이 위치하는 패키지입니다.
+ * 예: UserController, AuthController 등
+ */
+package com.example.Capstone_Design.controller;
---
+++ src/main/java/com/example/Capstone_Design/token/JwtAuthenticationFilter.java
@@ -0,0 +1,43 @@
+package com.example.Capstone_Design.token;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+
+    public JwtAuthenticationFilter(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    // Http 요청이 들어오면 가장 먼저 거치는 filter
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        // Header에서 Token 가져오기
+        String token = jwtProvider.resolveToken(request);
+        //logger.info("📥 Authorization Header: " + token);
+
+
+
+        // 토큰이 유효하다면,
+        if (token != null && jwtProvider.validateToken(token)) {
+            // 토큰으로부터 인증 정보를 받아
+            Authentication authentication = jwtProvider.getAuthentication(token);
+            // SecurityContext 객체에 저장
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            //logger.info("✅ 인증 성공, 사용자 정보 = " + authentication.getName());
+        }
+        else {
+            logger.info("❌ 인증 실패");
+        }
+
+        filterChain.doFilter(request, response); // 다음 filter 실행
+    }
+}
