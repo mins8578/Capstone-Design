8d1564a0318f8ad63b8de33c2b7c56b76dc68fd1
mins8578 <mins8578@naver.com>
2025-05-25 14:01:31 +0900

    게시글 로직 수정

--- src/main/java/com/example/Capstone_Design/Exception/MajorCodeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.Capstone_Design.Exception;
-
-public class MajorCodeNotFoundException extends RuntimeException {
-
-    public MajorCodeNotFoundException(String message) {
-        super(message);
-    }
-}
